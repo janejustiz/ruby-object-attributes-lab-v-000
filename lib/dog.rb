a7b81d8b133a549a4b5fb40799a887aa9b791334
Jane Justiz <janejustiz@gmail.com>
2018-06-05 02:26:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,15 +3,15 @@ class Dog
     @name
   end
 
-  def name=
-    @name
+  def name=(dogs_name)
+    @name=dogs_name
   end
 
   def breed
     @breed
   end
 
-  def breed=
-    @breed
+  def breed=(dogs_breed)
+    @breed=dogs_breed
   end
 end
