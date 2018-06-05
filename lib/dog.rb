6bc62e2f949fb38e4eff20c2bef72128c008f44f
Jane Justiz <janejustiz@gmail.com>
2018-06-05 01:03:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,13 @@
 class Dog
-  def name=(dog_name)
-    @this_dogs_name = dog_name
+  def initialize (name)
+    @name=name
   end
 
   def name
-    @this_dogs_name
+    @name
   end
 
-  def breed=(dog_breed)
-  this_dogs_breed = dog_breed
-  end
-
-  def breed
-    this_dogs_breed
+  def name=(new_name)
+    @name=new_name
   end
 end
-
-fido = Dog.new
-fido.name = "Fido"
-
-puts fido.name
