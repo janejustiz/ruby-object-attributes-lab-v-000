444b25ea43c152fdc84d8ff5c3bd86cc7e76bd65
Jane Justiz <janejustiz@gmail.com>
2018-06-05 01:58:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,14 @@
 class Dog
 
   def initialize(dogs_name)
-      @this_dogs_name = name
-    end
+    @this_dogs_name = name
+  end
 
-    def name
-      @this_dogs_name
-    end
+  def name
+    @this_dogs_name
+  end
 
   def name=(new_name)
     @this_dogs_name = new_name
   end
-
-
 end
