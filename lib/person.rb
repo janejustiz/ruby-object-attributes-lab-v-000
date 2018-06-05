f186af634f093599b8c8ad14f72674054047200e
Jane Justiz <janejustiz@gmail.com>
2018-06-05 02:23:56 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,17 @@
 class Person
-
-  def initialize(name)
-    @name = name
-  end
-
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
+  def name=
+    @name
   end
 
+  def job
+    @job
+  end
+
+  def job=
+    @job
+  end
 end
